0425ac8d90da22610a2da15ad5c6dfc469c30207
RemiCuxac <7034375+RemiCuxac@users.noreply.github.com>
2026-03-13 01:15:32 +0100

    Add script to remove interactive controls in Maya
    This script removes interactive controls from joints and transforms in Maya, ensuring proper matrix settings after disconnection.

---
+++ InteractiveControlsCreator/RemoveInteractiveControls.py
@@ -0,0 +1,32 @@
+from maya import cmds
+
+
+def remove_interactive_controls():
+    for bone in cmds.ls(type="joint"):
+        out = cmds.ls(cmds.listHistory(bone, future=True), type="parentConstraint")
+        for const in out:
+            if cmds.attributeQuery("isInteractiveSetup", node=const, exists=True):
+                cmds.delete(const)
+
+    # because we can't only remove the connection, we have to set the matrix right after disconnection to set it.
+    # sometimes, disconnecting a connection automatically set the input to 0
+    for skin in cmds.ls(type="skinCluster"):
+        # below: could also use for inConnection in cmds.getAttr(skin + ".bindPreMatrix', multiIndices=True)
+        for inConnection in cmds.ls(skin + ".bindPreMatrix[*]"):
+            mat = cmds.getAttr(inConnection)
+            inBoneConnection = cmds.connectionInfo(inConnection, sourceFromDestination=True)
+            if inBoneConnection:
+                cmds.disconnectAttr(inBoneConnection, inConnection)
+                cmds.setAttr(inConnection, mat, type="matrix")
+
+    for obj in cmds.ls(type="transform"):
+        try:
+            if cmds.attributeQuery("isInteractiveSetup", node=obj, exists=True):
+                cmds.delete(obj)
+        except TypeError as e:
+            # because if we delete top element, children will be deleted too
+            pass
+
+    cmds.select(clear=True)
+    
+remove_interactive_controls()
